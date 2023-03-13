8aba93e94fd1a6796bcbaba8beaa6c5a2c006f14
Aline Scholl Santos <76217058+aliinescholl@users.noreply.github.com>
2023-03-13 19:11:23 -0300

    Prototipação das telas

---
+++ 4 - Projeto/frontend/views/UfrmDigitacaoNota.pas
@@ -0,0 +1,42 @@
+unit UfrmDigitacaoNota;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, FMX.Edit, FMX.Objects;
+
+type
+  TForm2 = class(TForm)
+    Layout1: TLayout;
+    Label1: TLabel;
+    Layout2: TLayout;
+    Label2: TLabel;
+    lyt_nome_fantasia: TLayout;
+    Edit1: TEdit;
+    Rectangle1: TRectangle;
+    lyt_cnpj: TLayout;
+    Label3: TLabel;
+    Rectangle2: TRectangle;
+    Edit2: TEdit;
+    Layout3: TLayout;
+    Label4: TLabel;
+    Rectangle3: TRectangle;
+    Edit3: TEdit;
+    lyt_itens: TLayout;
+    Label5: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.fmx}
+
+end.
